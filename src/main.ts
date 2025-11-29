6d75d9a8ee7e80fc51b04edbf86c3c3b239b758c
andreyka101 <andrejkvasnin236@gmail.com>
2025-11-29 19:06:51 +0300

    wdwddwdw

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ num = 50
 // num = "ty"
 console.log(num);
 
-const num_const = 5
+// const num_const = 5
 // num_const = 10
 console.log(num);
 
@@ -19,6 +19,116 @@ console.log(num);
 // alert('text')
 // alert('text2')
 // alert('text3')
+
+
+
+
+// console.log(typeof(0));
+// console.log(typeof("stt"));
+// console.log(typeof(true));
+// console.log(Symbol("56"));
+// console.log(typeof(Symbol("56")));
+// console.log(typeof(Math));
+// console.log(typeof(alert));
+// let num_none1 = null
+// console.log(num_none1);
+
+// let num_none2
+// console.log(num_none2);
+
+// let num_none3 = 2/0
+// console.log(num_none3);
+
+// let num_none4 = "text" - 6
+// console.log(num_none4);
+
+
+
+
+// let num_if = 4
+// if(num_if == 5){
+//     console.log("ok1");
+// }
+// else if(num_if == 4){
+//     console.log("ok2");
+// }
+// else{
+//     console.log("else");
+// }
+
+
+
+
+
+// let num_if2:any = 4
+// if(num_if2 === "4"){
+//     console.log("ok1");
+// }
+// else{
+//     console.log("else");
+// }
+
+
+
+
+
+// let num_if3:any = 5
+// if(num_if3 == 3 || num_if3 < 10){
+//     console.log("ok1");
+// }
+// else{
+//     console.log("else");
+// }
+
+
+
+
+// let num_if4:any = 5
+// if(num_if4 == 3 && num_if4 < 10){
+//     console.log("ok1");
+// }
+// else{
+//     console.log("else");
+// }
+
+
+
+
+// let num_if5:any = 5
+// if(!(num_if5 == 3)){
+//     console.log("ok1");
+// }
+// else{
+//     console.log("else");
+// }
+
+
+
+
+// +
+// -
+// /
+// *
+// **
+// %
+
+
+
+
+
+console.log(prompt("num ?"));
+let num_prompt = prompt("num ?" , "69")
+console.log(num_prompt);
+
+
+
+console.log(confirm("yes ?"));
+
+
+
+
+
+
 
 
 
