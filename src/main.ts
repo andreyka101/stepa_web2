22309786565d31e5b1be5a81c2d9b8a16b9ab7ec
andreyka101 <andrejkvasnin236@gmail.com>
2025-11-29 18:09:51 +0300

    2у2у2у2у

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+// import './style.css'
+
+
+console.log("hello");
+
+
+// let num = 0
+// let num 
+let num : number
+num = 50
+// num = "ty"
+console.log(num);
+
+const num_const = 5
+// num_const = 10
+console.log(num);
+
+
+// alert('text')
+// alert('text2')
+// alert('text3')
+
+
+
+
+
